f6aba2227cadac45a01d9cc32f53066d2abbd7fa
James Brooks <kerspoon@gmail.com>
2013-01-21 19:35:02 +0000

    added more cases to sample size

--- sample-size.sh
+++ sample-size.sh
@@ -1,18 +1,36 @@
 #! /bin/bash
 
-python2 main.py outage 100 > out100.csv
+python main.py outage 100 > out100.csv
 
-python2 main.py failure 10 < out100.csv | python2 main.py simulate > fail10.csv
-python2 main.py analyse < fail10.csv > analysis10.csv
+python main.py failure 1 < out100.csv > fail1.csv
 
-python2 main.py failure 100 < out100.csv | python2 main.py simulate > fail100.csv
-python2 main.py analyse < fail100.csv > analysis100.csv
 
-python2 main.py failure 1000 < out100.csv | python2 main.py simulate > fail1000.csv
-python2 main.py analyse < fail1000.csv > analysis1000.csv
+python main.py failure 5 < out100.csv > fail5.csv
 
-python2 main.py failure 10000 < out100.csv | python2 main.py simulate > fail10000.csv
-python2 main.py analyse < fail10000.csv > analysis10000.csv
 
-python2 main.py failure 100000 < out100.csv | python2 main.py simulate > fail100000.csv
-python2 main.py analyse < fail100000.csv > analysis100000.csv
+python main.py failure 10 < out100.csv > fail10.csv
+
+
+python main.py failure 50 < out100.csv > fail50.csv
+
+
+python main.py failure 100 < out100.csv > fail100.csv
+
+
+python main.py failure 500 < out100.csv > fail500.csv
+
+
+python main.py failure 1000 < out100.csv > fail1000.csv
+
+
+python main.py failure 5000 < out100.csv > fail5000.csv
+
+
+python main.py failure 10000 < out100.csv > fail10000.csv
+
+
+python main.py failure 50000 < out100.csv > fail50000.csv
+
+
+python main.py failure 100000 < out100.csv > fail100000.csv
+
